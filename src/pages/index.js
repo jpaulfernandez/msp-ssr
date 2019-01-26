454c40e4e728fd8516cd2d700d97a1b84c51f8a4
jomelmartyguiao <mrjomelguiao@gmail.com>
2019-01-26 20:41:27 +0800

    new branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import {Navbar, Footer} from '../component';
-import {saveToDB} from '../services';
-
+import React from 'react';
+import { Navbar, Footer } from '../component';
+import { saveToDB } from '../services';
 
 export default class App extends React.Component {
-    componentDidMount() {
-        saveToDB('new user');
-    }
-    render() {
-        return(<div>this is homepage!</div>)
-    }
+	componentDidMount() {
+		saveToDB('new user');
+	}
+	render() {
+		return <div>this is navbar!</div>;
+	}
 }
